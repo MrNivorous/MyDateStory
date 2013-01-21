ca225645dcc6230eebbb5bade5a02ece8c454b8a
MrNivorous <karn.sood@gmail.com>
2013-01-20 19:56:27 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyDateStory::Application.config.secret_token = 'e15d17e0a90d18f8d3893387f2c0ee06efc2026250335f9bff914ad2ab2228bad5f8a843e7edcef956538fd0d68192d421a0477f15a8a4b4127e641dcf6cfefc'
